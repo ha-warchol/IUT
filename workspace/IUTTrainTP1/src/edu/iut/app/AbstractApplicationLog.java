8d3e8f1c9841da3924eb97c47e6655950d5a65ac
David Ha <davidha@MacBook-Pro-de-David.local>
2015-11-03 13:23:27 +0100

    commit

--- workspace/IUTTrainTP1/src/edu/iut/app/AbstractApplicationLog.java
+++ workspace/IUTTrainTP1/src/edu/iut/app/AbstractApplicationLog.java
@@ -6,11 +6,11 @@ public abstract class AbstractApplicationLog implements IApplicationLog {
 
 	protected String message;
 	/** TP1 : Tableau au sens des collections **/
-	protected /*TYPE TABLEAU*/ listeners;
+	protected ArrayList listeners;
 	
 	public AbstractApplicationLog() {
 		message = null;
-		listeners = new /*TYPE TABLEAU*/();
+		listeners = new ArrayList();
 	}
 	
     /** TP1 : Fonction venant de l'interface par héritage */
