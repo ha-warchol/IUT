2a0ed6f6c1cfadd175d4b4e9b4e56e9206cf6bf9
David Ha <davidha@MacBook-Pro-de-David.local>
2015-11-06 12:16:31 +0100

    Ajout de l'exercice 2

--- workspace/IUTTrainTP1/src/edu/iut/app/ApplicationLogs.java
+++ workspace/IUTTrainTP1/src/edu/iut/app/ApplicationLogs.java
@@ -10,17 +10,26 @@ public class ApplicationLogs extends ArrayList<IApplicationLog> {
 	
 	public ArrayList<IApplicationLog> getErrors() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
-		// une boucle à faire ici, penser à utiliser instanceof
+		for (IApplicationLog log_i : this ) {
+			if(log_i instanceof ApplicationErrorLog)
+				filteredLogs.add(log_i);
+		}
 		return filteredLogs;
 	}
 	public ArrayList<IApplicationLog> getWarnings() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
-		// une boucle à faire ici, penser à utiliser instanceof
+		for (IApplicationLog log_i : this) {
+			if(log_i instanceof ApplicationWarningLog)
+				filteredLogs.add(log_i);
+		}
 		return filteredLogs;
 	}
 	public ArrayList<IApplicationLog> getInfos() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
-		// une boucle à faire ici, penser à utiliser instanceof
+		for (IApplicationLog log_i : this) {
+			if(log_i instanceof ApplicationInfoLog)
+				filteredLogs.add(log_i);
+		}
 		return filteredLogs;
 	}
 	
--- workspace/IUTTrainTP1/src/edu/iut/app/ApplicationWarningLog.java
+++ workspace/IUTTrainTP1/src/edu/iut/app/ApplicationWarningLog.java
@@ -3,6 +3,7 @@ package edu.iut.app;
 public class ApplicationWarningLog extends AbstractApplicationLog {
 
 	/** Ajouter un tableau ou profiter de l'héritage ? */
+	/** Héritage avec appel du constructeur de la classe mère */
 	public ApplicationWarningLog() {
 		super();
 	}
